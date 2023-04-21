39d6a068af6c497fee9b346713cf56b20a7ee5b7
Afshin Emadi <work.afshinemadi@gmail.com>
2023-04-21 09:35:47 +0330

    change the NewUser component to be controlled component

--- src/components/Users/NewUser.jsx
+++ src/components/Users/NewUser.jsx
@@ -5,25 +5,34 @@ import Modal from "../UI/Modal";
 
 const NewUser = (props) => {
   const [errorMsg, setErrorMsg] = useState("");
+  const [inputName, setInputName] = useState("");
+  const [inputAge, setInputAge] = useState("");
 
-  const onSubmitHandler = (event) => {
+  const addUserHandler = (event) => {
     event.preventDefault();
 
-    const inputName = event.target[0].value;
-    const inputAge = event.target[1].value;
-    if (!inputName || !inputAge) {
+    if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
       setErrorMsg("Please enter a valid name and age(none-empty values).");
       return;
     }
-    else if (inputAge < 0) {
+    if (+inputAge < 1) {
       setErrorMsg("Please enter a valid age (>0).");
-      return;      
+      return;
     }
 
     props.onAdd({
       name: inputName,
       age: inputAge,
     });
+    setInputName("");
+    setInputAge("");
+  };
+
+  const nameChangeHandler = (event) => {
+    setInputName(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setInputAge(event.target.value);
   };
   const handleConfirm = () => {
     setErrorMsg("");
@@ -37,15 +46,26 @@ const NewUser = (props) => {
           onConfirm={handleConfirm}
         />
       )}
-      <form onSubmit={onSubmitHandler}>
+      <form onSubmit={addUserHandler}>
         <div>
           <div className={styles["form-input"]}>
-            <label>{"Username"}</label>
-            <input type="text" />
+            <label htmlFor="name">{"Username"}</label>
+            <input
+              id="name"
+              type="text"
+              value={inputName}
+              onChange={nameChangeHandler}
+            />
           </div>
           <div className={styles["form-input"]}>
-            <label>{"Age (Years)"}</label>
-            <input type="number" step={"1"} />
+            <label htmlFor="age">{"Age (Years)"}</label>
+            <input
+              id="age"
+              type="number"
+              step={"1"}
+              value={inputAge}
+              onChange={ageChangeHandler}
+            />
           </div>
         </div>
         <div>
